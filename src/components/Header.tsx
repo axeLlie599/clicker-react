7e2f9beac83e4d52b9ba866137ce161042ff94bd
AxelLee <sogachevsaho@gmail.com>
2025-02-10 18:02:16 +0300

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,25 @@
 import { motion } from "framer-motion";
+import { clsx } from "clsx";
 
 export default function Header() {
-    return (
-      <header className="flex flex-col w-full h-auto mx-auto gap-4 justify-center items-center pt-12">
-        <motion.span
-          className="text-4xl font-[300] text-gray-900/60 select-none"
-          initial={{ opacity: 0, y: -20 }}
-          animate={{
-            opacity: 1,
-            y: 0,
-            transition: { duration: 0.5, delay: 0.2 },
-          }}
-        >
-          Counter
-        </motion.span>
-      </header>
-    );
+  const headerStyles = clsx(
+    "flex flex-col gap-4",
+    "w-full h-auto mx-auto",
+    "justify-center items-center pt-12"
+  );
+  return (
+    <header className={headerStyles}>
+      <motion.span
+        className="text-4xl font-[300] text-gray-900/60 select-none"
+        initial={{ opacity: 0, y: -20 }}
+        animate={{
+          opacity: 1,
+          y: 0,
+          transition: { duration: 0.5, delay: 0.2 },
+        }}
+      >
+        <span>Counter</span>
+      </motion.span>
+    </header>
+  );
 }
